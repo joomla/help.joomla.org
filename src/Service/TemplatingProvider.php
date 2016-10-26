e349f85fbd713d277ae293ac8624bda7bc3f7787
Michael Babker <mbabker@flbab.com>
2016-10-25 21:33:46 -0500

    Split the template Closures into methods

--- src/Service/TemplatingProvider.php
+++ src/Service/TemplatingProvider.php
@@ -13,6 +13,8 @@ namespace Joomla\Help\Service;
 
 use Joomla\Cache\Item\Item;
 use Joomla\DI\Container;
+use Joomla\DI\ContainerAwareInterface;
+use Joomla\DI\ContainerAwareTrait;
 use Joomla\DI\ServiceProviderInterface;
 use Joomla\Http\HttpFactory;
 use Joomla\Renderer\PlatesRenderer;
@@ -22,8 +24,10 @@ use League\Plates\Engine;
 /**
  * Templating service provider
  */
-class TemplatingProvider implements ServiceProviderInterface
+class TemplatingProvider implements ServiceProviderInterface, ContainerAwareInterface
 {
+	use ContainerAwareTrait;
+
 	/**
 	 * Registers the service provider with a DI container.
 	 *
@@ -33,215 +37,188 @@ class TemplatingProvider implements ServiceProviderInterface
 	 */
 	public function register(Container $container)
 	{
+		$this->setContainer($container);
+
 		$container->alias('renderer', RendererInterface::class)
 			->share(
 				RendererInterface::class,
-				function (Container $container) {
-					// Add functions to the renderer
+				function (Container $container)
+				{
 					$engine = new Engine(JPATH_ROOT . '/templates');
 					$engine->addFolder('partials', JPATH_ROOT . '/templates/partials');
 
-					$engine->registerFunction(
-						'media',
-						function ($asset) use ($container)
-						{
-							return $container->get('app')->get('uri.media.full') . $asset;
-						}
-					);
-
-					$engine->registerFunction(
-						'current_url',
-						function () use ($container)
-						{
-							return $container->get('app')->get('uri.request');
-						}
-					);
-
-					$engine->registerFunction(
-						'cdn_menu',
-						function () use ($container)
-						{
-							/** @var \Psr\Cache\CacheItemPoolInterface $cache */
-							$cache = $container->get('cache');
-
-							$key = md5(get_class($this) . 'cdn_menu');
-
-							if ($cache->hasItem($key))
-							{
-								$item = $cache->getItem($key);
-
-								// Make sure we got a hit on the item, otherwise we'll have to re-cache
-								if ($item->isHit())
-								{
-									$body = $item->get();
-								}
-								else
-								{
-									try
-									{
-										// Set a very short timeout to try and not bring the site down
-										$response = (new HttpFactory)->getHttp()->get('https://cdn.joomla.org/template/renderer.php?section=menu', [], 2);
-
-										if ($response->code !== 200)
-										{
-											$body = 'Could not load template section.';
-										}
-										else
-										{
-											$body = $response->body;
-
-											// Remove the search module
-											$body = str_replace("\t<div id=\"nav-search\" class=\"navbar-search pull-right\">\n\t\t<jdoc:include type=\"modules\" name=\"position-0\" style=\"none\" />\n\t</div>\n", '', $body);
-
-											$item = (new Item($key, $container->get('app')->get('cache.lifetime', 900)))
-												->set($body);
-
-											$cache->save($item);
-										}
-									}
-									catch (\RuntimeException $e)
-									{
-										$body = 'Could not load template section.';
-									}
-								}
-							}
-							else
-							{
-								try
-								{
-									// Set a very short timeout to try and not bring the site down
-									$response = (new HttpFactory)->getHttp()->get('https://cdn.joomla.org/template/renderer.php?section=menu', [], 2);
-
-									if ($response->code !== 200)
-									{
-										$body = 'Could not load template section.';
-									}
-									else
-									{
-										$body = $response->body;
-
-										// Remove the search module
-										$body = str_replace("\t<div id=\"nav-search\" class=\"navbar-search pull-right\">\n\t\t<jdoc:include type=\"modules\" name=\"position-0\" style=\"none\" />\n\t</div>\n", '', $body);
-
-										$item = (new Item($key, $container->get('app')->get('cache.lifetime', 900)))
-											->set($body);
-
-										$cache->save($item);
-									}
-								}
-								catch (\RuntimeException $e)
-								{
-									$body = 'Could not load template section.';
-								}
-							}
-
-							return $body;
-						}
-					);
-
-					$engine->registerFunction(
-						'cdn_footer',
-						function () use ($container)
-						{
-							/** @var \Psr\Cache\CacheItemPoolInterface $cache */
-							$cache = $container->get('cache');
-
-							$key = md5(get_class($this) . 'cdn_footer');
-
-							if ($cache->hasItem($key))
-							{
-								$item = $cache->getItem($key);
-
-								// Make sure we got a hit on the item, otherwise we'll have to re-cache
-								if ($item->isHit())
-								{
-									$body = $item->get();
-								}
-								else
-								{
-									try
-									{
-										// Set a very short timeout to try and not bring the site down
-										$response = (new HttpFactory)->getHttp()->get('https://cdn.joomla.org/template/renderer.php?section=footer', [], 2);
-
-										if ($response->code !== 200)
-										{
-											$body = 'Could not load template section.';
-										}
-										else
-										{
-											$body = $response->body;
-
-											// Remove the login link
-											$body = str_replace("\t\t<li><a href=\"%loginroute%\">%logintext%</a></li>\n", '', $body);
-
-											// Replace the placeholders
-											$body = strtr(
-												$body,
-												[
-													'%reportroute%' => 'https://github.com/joomla/joomla-websites/issues/new?title=[jhelp]%20&amp;body=Please%20describe%20the%20problem%20or%20your%20issue',
-													'%currentyear%' => date('Y'),
-												]
-											);
-
-											$item = (new Item($key, $container->get('app')->get('cache.lifetime', 900)))
-												->set($body);
-
-											$cache->save($item);
-										}
-									}
-									catch (\RuntimeException $e)
-									{
-										$body = 'Could not load template section.';
-									}
-								}
-							}
-							else
-							{
-								try
-								{
-									// Set a very short timeout to try and not bring the site down
-									$response = (new HttpFactory)->getHttp()->get('https://cdn.joomla.org/template/renderer.php?section=footer', [], 2);
-
-									if ($response->code !== 200)
-									{
-										$body = 'Could not load template section.';
-									}
-									else
-									{
-										$body = $response->body;
-
-										// Remove the login link
-										$body = str_replace("\t\t<li><a href=\"%loginroute%\">%logintext%</a></li>\n", '', $body);
-
-										// Replace the placeholders
-										$body = strtr(
-											$body,
-											[
-												'%reportroute%' => 'https://github.com/joomla/joomla-websites/issues/new?title=[jhelp]%20&amp;body=Please%20describe%20the%20problem%20or%20your%20issue',
-												'%currentyear%' => date('Y'),
-											]
-										);
-
-										$item = (new Item($key, $container->get('app')->get('cache.lifetime', 900)))
-											->set($body);
-
-										$cache->save($item);
-									}
-								}
-								catch (\RuntimeException $e)
-								{
-									$body = 'Could not load template section.';
-								}
-							}
-
-							return $body;
-						}
-					);
+					// Add functions to the renderer
+					$engine->registerFunction('media', [$this, 'getMediaUrl']);
+					$engine->registerFunction('current_url', [$this, 'getCurrentUrl']);
+					$engine->registerFunction('cdn_menu', [$this, 'getCdnMenu']);
+					$engine->registerFunction('cdn_footer', [$this, 'getCdnFooter']);
 
 					return new PlatesRenderer($engine);
 				},
 				true
 			);
+	}
+
+	/**
+	 * Retrieve the template footer contents from the CDN.
+	 *
+	 * @return  string
+	 */
+	public function getCdnFooter() : string
+	{
+		/** @var \Psr\Cache\CacheItemPoolInterface $cache */
+		$cache = $this->getContainer()->get('cache');
+
+		$key = md5(get_class($this) . 'cdn_footer');
+
+		$remoteRequest = function () use ($cache, $key)
+		{
+			try
+			{
+				// Set a very short timeout to try and not bring the site down
+				$response = (new HttpFactory)->getHttp()->get('https://cdn.joomla.org/template/renderer.php?section=footer', [], 2);
+
+				if ($response->code !== 200)
+				{
+					return 'Could not load template section.';
+				}
+
+				$body = $response->body;
+
+				// Remove the login link
+				$body = str_replace("\t\t<li><a href=\"%loginroute%\">%logintext%</a></li>\n", '', $body);
+
+				// Replace the placeholders
+				$body = strtr(
+					$body,
+					[
+						'%reportroute%' => 'https://github.com/joomla/joomla-websites/issues/new?title=[jhelp]%20&amp;body=Please%20describe%20the%20problem%20or%20your%20issue',
+						'%currentyear%' => date('Y'),
+					]
+				);
+
+				$item = (new Item($key, $this->getContainer()->get('app')->get('cache.lifetime', 900)))
+					->set($body);
+
+				$cache->save($item);
+
+				return $body;
+			}
+			catch (\RuntimeException $e)
+			{
+				return 'Could not load template section.';
+			}
+		};
+
+		if ($cache->hasItem($key))
+		{
+			$item = $cache->getItem($key);
+
+			// Make sure we got a hit on the item, otherwise we'll have to re-cache
+			if ($item->isHit())
+			{
+				$body = $item->get();
+			}
+			else
+			{
+				$body = $remoteRequest();
+			}
+		}
+		else
+		{
+			$body = $remoteRequest();
+		}
+
+		return $body;
+	}
+
+	/**
+	 * Retrieve the template mega menu from the CDN.
+	 *
+	 * @return  string
+	 */
+	public function getCdnMenu() : string
+	{
+		/** @var \Psr\Cache\CacheItemPoolInterface $cache */
+		$cache = $this->getContainer()->get('cache');
+
+		$key = md5(get_class($this) . 'cdn_menu');
+
+		$remoteRequest = function () use ($cache, $key)
+		{
+			try
+			{
+				// Set a very short timeout to try and not bring the site down
+				$response = (new HttpFactory)->getHttp()->get('https://cdn.joomla.org/template/renderer.php?section=menu', [], 2);
+
+				if ($response->code !== 200)
+				{
+					return 'Could not load template section.';
+				}
+
+				$body = $response->body;
+
+				// Remove the search module
+				$body = str_replace(
+					"\t<div id=\"nav-search\" class=\"navbar-search pull-right\">\n\t\t<jdoc:include type=\"modules\" name=\"position-0\" style=\"none\" />\n\t</div>\n",
+					'',
+					$body
+				);
+
+				$item = (new Item($key, $this->getContainer()->get('app')->get('cache.lifetime', 900)))
+					->set($body);
+
+				$cache->save($item);
+
+				return $body;
+			}
+			catch (\RuntimeException $e)
+			{
+				return 'Could not load template section.';
+			}
+		};
+
+		if ($cache->hasItem($key))
+		{
+			$item = $cache->getItem($key);
+
+			// Make sure we got a hit on the item, otherwise we'll have to re-cache
+			if ($item->isHit())
+			{
+				$body = $item->get();
+			}
+			else
+			{
+				$body = $remoteRequest();
+			}
+		}
+		else
+		{
+			$body = $remoteRequest();
+		}
+
+		return $body;
+	}
+
+	/**
+	 * Retrieve the current URL.
+	 *
+	 * @return  string
+	 */
+	public function getCurrentUrl() : string
+	{
+		return $this->getContainer()->get('app')->get('uri.request');
+	}
+
+	/**
+	 * Get the URL for a media asset.
+	 *
+	 * @param   string  $asset  The asset to build the URL for.
+	 *
+	 * @return  string
+	 */
+	public function getMediaUrl($asset) : string
+	{
+		return $this->getContainer()->get('app')->get('uri.media.full') . $asset;
 	}
 }
