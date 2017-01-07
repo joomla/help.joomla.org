376233c5e6bb046256dfa6aa57c4e4ba6dd45f7c
Michael Babker <mbabker@flbab.com>
2017-01-07 11:24:47 -0600

    2017

--- src/Router.php
+++ src/Router.php
@@ -2,7 +2,7 @@
 /**
  * Joomla! Help Site
  *
- * @copyright  Copyright (C) 2016 Open Source Matters, Inc. All rights reserved.
+ * @copyright  Copyright (C) 2016 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
  *
  * Portions of this code are derived from the previous help screen proxy component,
