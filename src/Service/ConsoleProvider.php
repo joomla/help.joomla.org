dec8ddf4086196b94d755e2a6ce98e6e3f8da89d
Michael Babker <michael.babker@gmail.com>
2020-02-23 13:16:18 -0600

    Bootstrap Console

---
+++ src/Service/ConsoleProvider.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Joomla! Help Site
+ *
+ * @copyright  Copyright (C) 2016 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ *
+ * Portions of this code are derived from the previous help screen proxy component,
+ * please see https://github.com/joomla-projects/help-proxy for attribution
+ */
+
+namespace Joomla\Help\Service;
+
+use Joomla\Console\Application;
+use Joomla\Console\Loader\ContainerLoader;
+use Joomla\Console\Loader\LoaderInterface;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+/**
+ * Console service provider
+ */
+class ConsoleProvider implements ServiceProviderInterface
+{
+	/**
+	 * Registers the service provider with a DI container.
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  void
+	 */
+	public function register(Container $container)
+	{
+		$container->share(
+			Application::class,
+			static function (Container $container): Application
+			{
+				$application = new Application(new ArgvInput, new ConsoleOutput, $container->get('config'));
+
+				$application->setCommandLoader($container->get(LoaderInterface::class));
+				$application->setDispatcher($container->get(DispatcherInterface::class));
+				$application->setLogger($container->get(LoggerInterface::class));
+				$application->setName('Joomla! Help Website');
+
+				return $application;
+			},
+			true
+		);
+
+		$container->alias(ContainerLoader::class, LoaderInterface::class)
+			->share(
+				LoaderInterface::class,
+				static function (Container $container): LoaderInterface
+				{
+					$mapping = [];
+
+					return new ContainerLoader($container, $mapping);
+				},
+				true
+			);
+	}
+}
